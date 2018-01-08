8c824c795e4fdc5352157b862039e922e3cf14aa
amythpaddy <amythpaddy21@gmail.com>
2018-01-08 18:24:44 +0530

    Commit #1

---
+++ app/src/main/java/com/example/amyth_shekhar/tutor/GlobalPathPoints.java
@@ -0,0 +1,42 @@
+package com.example.amyth_shekhar.tutor;
+
+/**
+ * Created by mac on 05/01/18.
+ */
+
+public class GlobalPathPoints {
+    static int scale = 5;
+    public static int[][] a_path = {{15*scale+50,50+scale*5},
+            {15*scale+50,50+scale*90},
+            {35*scale+50,50+scale*90},
+            {35*scale+50,50+scale*65},
+            {85*scale+50,50+scale*65},
+            {85*scale+50,50+scale*90},
+            {105*scale+50,50+scale*90},
+            {105*scale+50,50+scale*5},
+            {15*scale+50,50+scale*5},
+            {35*scale+50,50+scale*20},
+            {35*scale+50,50+scale*45},
+            {85*scale+50,50+scale*45},
+            {85*scale+50,50+scale*20},
+            {35*scale+50,50+scale*20}};
+
+    public static int[][] points = {{25*scale,90*scale},
+            {25*scale,75*scale},
+            {25*scale,65*scale},
+            {25*scale,55*scale},
+            {25*scale,40*scale},
+            {25*scale,35*scale},
+            {25*scale,20*scale},
+            {25*scale,10*scale},
+            {95*scale,10*scale},
+            {95*scale,25*scale},
+            {95*scale,40*scale},
+            {95*scale,55*scale},
+            {95*scale,70*scale},
+            {95*scale,85*scale},
+            {95*scale,90*scale},
+            {25*scale,55*scale},
+            {60*scale,55*scale},
+            {95*scale,55*scale}};
+}
